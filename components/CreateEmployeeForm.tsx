8a973816f049928b147e2ae5e9d62f79f49e41a3
Miraku17 <zhaztedv@gmail.com>
2025-11-23 00:21:18 +0800

    fixed import error

--- components/CreateEmployeeForm.tsx
+++ components/CreateEmployeeForm.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { UserProfile } from '../../types';
-
+import { UserProfile } from '@/types';
 interface CreateEmployeeFormProps {
   onEmployeeCreated: (employee: UserProfile) => void;
 }
